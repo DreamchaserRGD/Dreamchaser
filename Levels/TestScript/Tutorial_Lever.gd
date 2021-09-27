79724a4858d982464c680b5c994e23962e8e2253
Simon <schmitt-simon@outlook.de>
2021-09-27 18:36:56 +0200

    Cleanup

--- Levels/TestScript/Tutorial_Lever.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var root
-
-func enterTrigger(body):
-	if body.get_name() == "Player":
-		print("Hebel betätigt!")
-		root = get_tree().get_root().get_node("SceneManager")
-		root.leverGetPulled()
