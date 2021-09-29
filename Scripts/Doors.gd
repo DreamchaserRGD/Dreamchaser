97e3646f49b424a989c1975d42a7b49a2b4bb31b
Simon <schmitt-simon@outlook.de>
2021-09-29 19:37:33 +0200

    Auto stash before merge of "Level_design" and "origin/Level_design"
    interact with e instead of jump

--- Scripts/Doors.gd
+++ Scripts/Doors.gd
@@ -12,7 +12,7 @@ func _ready():
 
 func _physics_process(delta):
 	for body in area.get_overlapping_bodies():
-		if body.name == "Player" and Input.is_action_just_pressed("ui_up"):
+		if body.name == "Player" and Input.is_action_just_pressed("interact"):
 			G.next_level_door = door_name
 			G.next_level_number = level_number
 			G.real_or_dream = "real"
